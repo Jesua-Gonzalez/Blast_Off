5a0486b26b6f6c8067c9586ed51ff12315caa17a
Jesua-Gonzalez <jesua.g0529@gmail.com>
2025-04-20 01:33:32 -0400

    Spacecraft Class Created

--- Blast_Off.py
+++ Blast_Off.py
@@ -24,3 +24,27 @@ class Astronaut:
 
 
 
+class Spacecraft:
+    def __init__(self, name, capacity):     #this class is defined with the name, capacity, and crew list of onboard astronauts 
+        self.name = name
+        self.capacity = capacity
+        self.crew = [] 
+    
+
+    def add_astronaut(self, *astronauts):  #simply add astornauts if amount in crew is less than the capacity
+        for astronaut in astronauts:
+            if len(self.crew) >= self.capacity:
+                print(f"Max Capacity Reached! {astronaut.name}, cannot aboard the mothership.")
+            else:
+                self.crew.append(astronaut)
+
+
+    def list_crew(self):
+        return [astronaut.name for astronaut in self.crew]
+    
+
+    def get_info(self):      #this section will store the spacecraft info in a dictionary
+        return { "name": self.name,
+                 "capacity": self.capacity,
+                 "crew": self.list_crew() }
+  
